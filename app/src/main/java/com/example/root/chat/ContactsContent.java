a60e18434a86acdd3a1c7907b633e618412c4551
winterssky <ik15209@etf.unsa.ba>
2015-02-18 14:02:08 +0100

    socket dodan

---
+++ app/src/main/java/com/example/root/chat/ContactsContent.java
@@ -0,0 +1,48 @@
+package com.example.root.chat;
+
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.ContactsContract;
+
+public class ContactsContent {
+
+    ContentResolver contentResolver;
+
+    public ContactsContent(ContentResolver cr) {
+        contentResolver = cr;
+    }
+
+    public Uri fetchContacts(String nameContact) {
+
+        Uri CONTENT_URI = ContactsContract.Contacts.CONTENT_URI;
+        String _ID = ContactsContract.Contacts._ID;
+        String DISPLAY_NAME = ContactsContract.Contacts.DISPLAY_NAME;
+        String HAS_PHONE_NUMBER = ContactsContract.Contacts.HAS_PHONE_NUMBER;
+
+        Uri PhoneCONTENT_URI = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+        String Phone_CONTACT_ID = ContactsContract.CommonDataKinds.Phone.CONTACT_ID;
+        String NUMBER = ContactsContract.CommonDataKinds.Phone.NUMBER;
+
+        Cursor cursor = contentResolver.query(CONTENT_URI, null, null, null, null);
+
+        // Loop for every contact in the phone
+        if (cursor.getCount() > 0) {
+
+            while (cursor.moveToNext()) {
+
+                String contact_id = cursor.getString(cursor.getColumnIndex(_ID));
+                String name = cursor.getString(cursor.getColumnIndex(DISPLAY_NAME));
+
+                int hasPhoneNumber = Integer.parseInt(cursor.getString(cursor.getColumnIndex(HAS_PHONE_NUMBER)));
+
+                if (hasPhoneNumber > 0 && nameContact.equals(name)) {
+
+                    Uri imageURI = Uri.withAppendedPath(ContactsContract.Contacts.CONTENT_URI, contact_id);
+                    return imageURI;
+                }
+            }
+        }
+        return null;
+    }
+}
