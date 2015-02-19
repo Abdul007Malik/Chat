dd8844f56d8be6314476aa94f9131e8b0b2a0f7e
winterssky <ik15209@etf.unsa.ba>
2015-02-19 12:49:15 +0100

    socket dodan Kontakti povlace slike

--- app/src/main/java/com/example/root/chat/ContactAdapter.java
+++ app/src/main/java/com/example/root/chat/ContactAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.provider.ContactsContract;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -26,11 +27,13 @@ public class ContactAdapter extends ArrayAdapter<Contact> {
     private Context context;
     private Contact contactObj;
     private ArrayList<Contact> contacts;
+    private InputStream inputStream;
 
     public ContactAdapter(Context c, ArrayList<Contact> contacts) {
         super(c, R.layout.contact_row, contacts);
         this.context = c;
         this.contacts = contacts;
+
     }
 
     class MyViewHolder {
@@ -78,27 +81,48 @@ public class ContactAdapter extends ArrayAdapter<Contact> {
         }
 
         // Avatar korisnika
-        //holder.avatar.setBackgroundResource(R.drawable.ic_contact_picture);
         String stringUri = contactObj.getImageUri();
         Uri imageUri = Uri.parse(stringUri);
         Log.d("uri", String.valueOf(imageUri));
 
-        // Potrebno podesiti da ide ASYNC TASK
-        // http://stackoverflow.com/questions/15012619/displaying-a-image-when-no-contact-image-is-available
-        InputStream inputStream = ContactsContract.Contacts.openContactPhotoInputStream(getContext().getContentResolver(), imageUri);
-        BufferedInputStream buff = new BufferedInputStream(inputStream);
-        Bitmap bitmap = BitmapFactory.decodeStream(buff);
-
-        if ("null".equals(stringUri) || bitmap == null) {
-            bitmap = BitmapFactory.decodeResource(getContext().getResources(), R.drawable.ic_contact_picture);
-            holder.avatar.setImageBitmap(bitmap);
-            //holder.avatar.setBackgroundResource(R.drawable.ic_contact_picture);
+        if ("null".equals(stringUri)) {
+            holder.avatar.setImageBitmap(BitmapFactory.decodeResource(getContext().getResources(), R.drawable.ic_contact_picture));
         } else {
-            holder.avatar.setImageBitmap(bitmap);
+            new ImageLoader(imageUri, holder).execute();
         }
+
+
 
         // Datum posljednje primljene poruke
         holder.msgDate.setText(contactObj.getMsgDate());
         return row;
+    }
+
+    private class ImageLoader extends AsyncTask<Void, Void, Bitmap> {
+
+        private Uri imageUri;
+        private MyViewHolder holder;
+
+        public ImageLoader(Uri imageUri, MyViewHolder holder) {
+            this.imageUri = imageUri;
+            this.holder = holder;
+        }
+
+        @Override
+        protected Bitmap doInBackground(Void... params) {
+            inputStream = ContactsContract.Contacts.openContactPhotoInputStream(getContext().getContentResolver(), imageUri);
+            BufferedInputStream buff = new BufferedInputStream(inputStream);
+            Bitmap bitmap = BitmapFactory.decodeStream(buff);
+            if (bitmap == null) {
+                bitmap = BitmapFactory.decodeResource(getContext().getResources(), R.drawable.ic_contact_picture);
+            }
+
+            return bitmap;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            holder.avatar.setImageBitmap(bitmap);
+        }
     }
 }
