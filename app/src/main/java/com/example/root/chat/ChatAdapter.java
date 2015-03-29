886c79a8953c8b55ed305a2dca4ef3883e191d17
winterssky <ik15209@etf.unsa.ba>
2015-03-29 01:26:03 +0100

    potrebno srediti datume poruka

--- app/src/main/java/com/example/root/chat/ChatAdapter.java
+++ app/src/main/java/com/example/root/chat/ChatAdapter.java
@@ -42,7 +42,7 @@ public class ChatAdapter extends ArrayAdapter<Message> {
         View row = convertView;
 
         if (row == null) {
-            row = LayoutInflater.from(context).inflate(R.layout.message_row_date, parent, false);
+            row = LayoutInflater.from(context).inflate(R.layout.message_row, parent, false);
             holder = new MyViewHolder(row);
             row.setTag(holder);
         } else {
